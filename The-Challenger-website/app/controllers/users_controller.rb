4149167aa9c3738adbd79714f74dd27418d72465
marinaeldib <marinaeldib@gmail.com>
2015-04-02 08:30:21 +0200

    Modified minor things but still missing the tests #15 #17

--- The-Challenger-website/app/controllers/users_controller.rb
+++ The-Challenger-website/app/controllers/users_controller.rb
@@ -4,17 +4,17 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
    end
 
-   	def new 
-   		@user = User.new
-   	end
+   	# def new 
+   	# 	@user = User.new
+   	# end
 
-   	def create
-   		@user = User.new(params[:user])
-   		if @user.save
-   			redirect_to @user
-   		else 
-   			render 'new'
-   		end
-   	end
+   	# def create
+   	# 	@user = User.new(params[:user])
+   	# 	if @user.save
+   	# 		redirect_to @user
+   	# 	else 
+   	# 		render 'new'
+   	# 	end
+   	# end
 
 end
--- The-Challenger-website/app/models/user.rb
+++ The-Challenger-website/app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-# def full_name
-#     if self.first_name.blank? && self.last_name.blank?
-#       self.email
-#     else
-#       self.first_name + " " + self.last_name
-#     end
-#   end
+ # :confirmable, :lockable, :timeoutable and :omniauthable
+def full_name
+    if self.first_name.blank? && self.last_name.blank?
+      self.email
+    else
+      self.first_name << " " << self.last_name
+    end
+  end
 
 
   devise :database_authenticatable, :registerable,
