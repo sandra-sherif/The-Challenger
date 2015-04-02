67309335bba323b6d4e4de7bcae65442a603c40c
Sandra S. Menkarios <sandra.menkarioss@gmail.com>
2015-04-02 16:05:02 +0200

    Changes

--- The-Challenger-website/db/migrate/20150401084837_create_challenges.rb
+++
@@ -1,9 +0,0 @@
-class CreateChallenges < ActiveRecord::Migration
-  def change
-    create_table :challenges do |t|
-      t.string :Path
-
-      t.timestamps
-    end
-  end
-end
--- The-Challenger-website/db/migrate/20150401085257_create_friends.rb
+++
@@ -1,9 +0,0 @@
-class CreateFriends < ActiveRecord::Migration
-  def change
-    create_table :friends do |t|
-      t.string :Status
-
-      t.timestamps
-    end
-  end
-end
