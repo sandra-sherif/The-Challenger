54d0329f768ec500c21ed1302fd475e76c66a5b3
marinaeldib <marinaeldib@gmail.com>
2015-04-04 17:51:18 +0200

    fixed the bugs in the profile picture still missing the tests

--- The-Challenger-website/app/models/user.rb
+++ The-Challenger-website/app/models/user.rb
@@ -16,7 +16,7 @@ def full_name
 	has_many :challenges, :dependent => :destroy
 	has_many :comments, :dependent => :destroy
 	has_many :Likes, :dependent => :destroy
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => 'The-Challenger-website/public/images/:style/missing.jpeg'
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "missing.jpeg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
