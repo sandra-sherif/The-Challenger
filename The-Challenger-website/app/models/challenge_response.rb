85141b4f9539af349d6eeed5f553b4f99b91f9e2
christianne-iskandar <christianne.iskandar@gmail.com>
2015-04-04 21:34:15 +0200

    added has many tags in challenge_response file

--- The-Challenger-website/app/models/challenge_response.rb
+++ The-Challenger-website/app/models/challenge_response.rb
@@ -1,5 +1,6 @@
 class ChallengeResponse < ActiveRecord::Base
 	belongs_to :User, :class_name => "User", :foreign_key => "user_id"
 	belongs_to :Challenge, :class_name => "Challenge", :foreign_key => "Challenge_id"
+	has_many :tags
 
 end
