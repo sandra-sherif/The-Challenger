d11706a5328ce88666f78efd10444eb7aedeae86
seifmeligy <seif.meligy@gmail.com>
2015-04-05 11:48:10 +0200

    change in the user_friendships_controller.rb

--- The-Challenger-website/app/controllers/user_friendships_controller.rb
+++ The-Challenger-website/app/controllers/user_friendships_controller.rb
@@ -1,8 +1,13 @@
 class UserFriendshipsController <ApplicationController
 	before_filter :authenticate_user!, only: [:new]
+
+	 
+	#this code is the controller of sending request to make a new friendship by using the friend ID and making sure that it is the correct friend
+	#If the friend is not selected it sends a flash message to choose a friend
+
 	def new
 		if params [:friend_id]
-			@friend = User.where(user_name: (params[:friend_id]).first
+			@friend = User.where(email: (params[:friend_id]).first
 				raise ActiveRecord::RecordNotFound if @friend.nil?
 			@user_friendship = current_user.user_friendships.new(friend: @friend)
 		else
@@ -10,13 +15,21 @@ class UserFriendshipsController <ApplicationController
 			flash[:error] = "Choose a Friend"
 			
 		end
+
+		#Here if the the friend ID is invaild it is caught by the test to direct it to Not found page
 		rescue ActiveRecord::RecordNotFound
 			render file: 'public/404' , status: :not_found
 
 	end
+
+
+#this code is used to create the friendship between the 2 users by saving the friendship and gives you a flash msg that the friend is added
+#then it directs to the profile path of the friend
+ 
+
 	def create
 		if params [:user_friendship] && params [:user_friendship].has_key?(:friend_id)
-			@friend = User.where(user_name:params [:user_friendship] [:friend_id]).first
+			@friend = User.where(email:params [:user_friendship] [:friend_id]).first
 			@user_friendship = current_user.user_friendships.new(friend: @friend)
 			@user_friendship.save
 			flash[:success] = "you are now friends with #{@friend.full_name}"
--- The-Challenger-website/app/models/user.rb
+++ The-Challenger-website/app/models/user.rb
@@ -18,8 +18,8 @@ def full_name
 	has_many :Likes, :dependent => :destroy
 has_many :addfriends
 has_many :friends, through: Addfriend
-
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "missing.jpeg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 
 end
