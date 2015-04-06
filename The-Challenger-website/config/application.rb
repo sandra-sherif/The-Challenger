c5c976a3ee5f4e17793dcb56ec0a5adc11bf5664
marinaeldib <marinaeldib@gmail.com>
2015-04-06 13:44:36 +0200

    added two buttons to profile #15

--- The-Challenger-website/config/application.rb
+++ The-Challenger-website/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Tagroba
+module TheChallenger
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -21,6 +21,19 @@ module Tagroba
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+   
+    
+config.generators do |g|
+  g.test_framework :rspec,
+    :fixtures => true,
+    :view_specs => false,
+    :helper_specs => false,
+    :routing_specs => false,
+    :controller_specs => true,
+    :request_specs => true
+g.fixture_replacement :factory_girl, dir: "spec/factories"
+end
+ config.active_record.raise_in_transactional_callbacks = true
   end
+
 end
---
+++ The-Challenger-website/spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+# require 'factory_girl_rails'
+# require 'spec_helper'
+# require 'rails_helper'
+
+# describe User do 
+# 	 it "has a valid factory" do
+# 	 Factory.create(:user).should be_valid
+# 	end
+# 	 it "is valid without a first_name"
+# 	 it "is valid without a last_name"
+# 	 it "is valid without an age"
+# 	 it "is valid without an avatar"
+# 	 it "is invalid without an email"
+# 	 it "returns a user's full_name as a string"
+# 	end
