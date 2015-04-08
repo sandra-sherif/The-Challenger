6bb4e930474aa3af49f5b35ee85b9950f4598d61
Amr Nafie <amr.nafie@gmail.com>
2015-04-09 00:48:24 +0200

    Code reviewing changes

--- The-Challenger-website/app/models/challenge.rb
+++ The-Challenger-website/app/models/challenge.rb
@@ -1,13 +1,13 @@
 class Challenge < ActiveRecord::Base
 	
-	# A challenge has user1_id and user2_id attributes as foreign keys from table User.
-	# A challenge has many comments.
-	# The model uses the Uploader class that was generated in order to get the path of the uploaded challenge.
-	# In order to create a new challenge the user must provide name, upload_type and path,
-	belongs_to :user1, :class_name => "User", :foreign_key => "user1_id"
-	belongs_to :user2, :class_name => "User", :foreign_key => "user2_id"
-	has_many :comments
-	mount_uploader :path, UploadUploader
+    # A challenge has user1_id and user2_id attributes as foreign keys from table User.
+    # A challenge has many comments.
+    # The model uses the Uploader class that was generated in order to get the path of the uploaded challenge.
+    # In order to create a new challenge the user must provide name, upload_type and path,
+    belongs_to :user1, :class_name => "User", :foreign_key => "user1_id"
+    belongs_to :user2, :class_name => "User", :foreign_key => "user2_id"
+    has_many :comments
+    mount_uploader :path, UploadUploader
     validates :name, presence: true
     validates :upload_type, presence: true
     validates :path, presence: true
