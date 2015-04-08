e33ba69dfe968c91551ba7f75cabf2863ec3b775
Sandra S. Menkarios <sandra.menkarioss@gmail.com>
2015-04-08 20:29:40 +0200

    #21

--- The-Challenger-website/test/controllers/comments_controller_test.rb
+++ The-Challenger-website/test/controllers/comments_controller_test.rb
@@ -4,4 +4,20 @@ class CommentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:challenge)
+  end
+
+  test "shows comment" do
+   get(:show, {'comment_id' => 1}, {'user_id' => 5}, {'challenge_id' => 1}) 
+    assert_response :success
+  end
+
+#  test "destroys comment" do
+ #   assert_difference('Challenge.count', -1) do
+  #    delete :destroy, id: @comment.id
+
 end
