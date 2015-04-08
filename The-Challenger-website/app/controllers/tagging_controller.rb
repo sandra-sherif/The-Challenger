e99106ffc9ecf1c2fc397b3c76af877bb5aa9a08
christianne-iskandar <christianne.iskandar@gmail.com>
2015-04-08 23:45:30 +0200

    changed the contoller, removed show as it does the same thing as index and removed new since create is new and save at the same time

--- The-Challenger-website/app/controllers/tagging_controller.rb
+++ The-Challenger-website/app/controllers/tagging_controller.rb
@@ -1,27 +1,25 @@
 class TaggingController < ApplicationController
 before_filter :signed_in_user, only: [:create, :destroy]
 	
-def index
+def index #to list all challenges
  @challenges = Challenge.find(params[:challenge_id])
  end
 
-def new
-		@tag = tag.new
-	end
+# def new
+# 		@tag = tag.new
+# 	end
 
-
+# to create a new challenge
 	def create
 		@challenge = Challenge.find(params[:challenge_id])
-		@tag.challenge = @challenge.tag.create(tag_params)
+		@tag.challenge = @challenge.tag.create(params[:challenge_id])
 		tag.save
     redirect_to challenge_path(@challenge)
 end
 
-def show 
-  @tag= Tag.find(params[:id])
-end
-private
-def tag_params
-  params.require(:tag).permit(:text)
-  end
+# def show #to show challenges 
+#   @tag= Tag.find(params[:id])
+# end
+
+
 end
