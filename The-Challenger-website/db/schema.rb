bd597e3f1512d97740258b34169e7ae073b8f332
Amr Nafie <amr.nafie@gmail.com>
2015-04-11 14:54:14 +0200

    Finished as an admin i should be able to delete comments story.

--- The-Challenger-website/db/schema.rb
+++ The-Challenger-website/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404140648) do
+ActiveRecord::Schema.define(version: 20150411124247) do
 
   create_table "challenge_responses", force: :cascade do |t|
     t.string   "path"
@@ -51,12 +51,12 @@ ActiveRecord::Schema.define(version: 20150404140648) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150404140648) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
