6951ed537e4c8d28000d7aff671cae32ef0fd126
Amr Nafie <amr.nafie@gmail.com>
2015-04-21 14:17:25 +0200

    Finished as a user i should be able to reply on a challenge #36

--- The-Challenger-website/app/models/response.rb
+++ The-Challenger-website/app/models/response.rb
@@ -2,7 +2,7 @@ class Response < ActiveRecord::Base
 
 	belongs_to :user1, :class_name => "User", :foreign_key => "user_id"
 	belongs_to :challenge, :class_name => "Challenge", :foreign_key => "challenge_id"
-	mount_uploader :path, UploadUploader
+	mount_uploader :path, ResponseUploader
     validates :name, presence: true
     validates :upload_type, presence: true
     validates :path, presence: true
