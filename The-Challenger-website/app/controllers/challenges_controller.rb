30bacc5367f6b05de84a1338dfb1ff1dc7d88e16
christianne-iskandar <christianne.iskandar@gmail.com>
2015-04-28 23:34:50 +0300

    adding in controller and views + to be able to retrieve everything

--- The-Challenger-website/app/controllers/challenges_controller.rb
+++ The-Challenger-website/app/controllers/challenges_controller.rb
@@ -1,29 +1,25 @@
 class ChallengesController < ApplicationController
 before_filter :set_search
 
- def set_search
-   @search = Challenge.search(params[:q])
- end
-# def index returns all the challenges in the database.
-def index
-  if :sharing_type == "Public"
-  @challenges = Challenge.all
-else show
-end
-  if params[:q]
-   @search = Challenge.search(params[:q])
-    @challenges = @search.result
+  def set_search
+    @search = Challenge.search(params[:q])
   end
-end
+  # def index returns all the challenges in the database.
+  def index
+    @challenges = Challenge.all
+    if params[:q]
+      @search = Challenge.search(params[:q])
+      @challenges = @search.result
+    end
+  end
 
-def new
+  def new
     @challenge = Challenge.new
-end
+  end
 
   # def create takes params from the user.
   # Creates a new challenge with the given params then redirects to challanges view.
   # View a message that the challenge is uploaded if the challenge is actually saved or render the same view.
-
   def create
     @challenge = Challenge.new(challenge_params)
     @challenge.user1_id = current_user.id
@@ -37,29 +33,41 @@ end
   # def destroy deletes a challange with the input challenge id.
   # Redirects to the challenges view with a message that the file is deleted.
   def destroy
-  	@challenge = Challenge.find(params[:id])
+    @challenge = Challenge.find(params[:id])
     @challenge.destroy
     redirect_to challenges_path, notice:  "The challenge #{@challenge.name} has been deleted."
   end
 
-# def show view a challenge with the input challenge id.
-   def show
-    @challenge = Challenge.find(params[:user_id && :sharing_with])
+  # def show view a challenge with the input challenge id.
+  def show
+    @challenge = Challenge.find(params[:id])
   end
 
+def indexPrivateSharing
+  if params[:sharing_type].value == "Private"
+    @challenges = Challege.where(owner_id: current_user.following_ids, owner_type: "User", | :sharing_with current_user)
+  else @challenge = Challenge.all
+end
 
-# def hide
-#   if params[:sharing_type] == "Public"
-#     self.class.index
-#   else 
-#     self.class.show
-#   end
-#  end
+PublicActivity::Activity.not_private.order("created_at desc").where(owner_id: current_user.following_ids, owner_type: "User"
+
+  #Default show Image method streams the file contents.
+  #File doesn't have to be in public/ dir
+  # def show
+  #   send_file @image.filename, :type => @image.content_type,
+  #             :disposition => 'inline'
+  # end
+
+  
+ 
   
  # Allows the view to access these attributes.
   private
   def challenge_params
     params.require(:challenge).permit(:name, :path, :user_id, :upload_type, :sharing_type, :sharing_with)
   end
+# Use your favorite authorization system to restrict access
+# filter_access_to :indexPrivateSharing, :require => :view, attribute_check => :true
+# end               #:show,
 end
-
+end
